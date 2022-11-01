5b83f76164d8b04f9b9d8a86d3a0d3fc9c781eb3
bernkastel15 <redpangilinan715@gmail.com>
2022-11-02 06:49:55 +0800

    Update toggle.js

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,6 +1,8 @@
 const navLinks = document.querySelectorAll('.nav-item')
 const menuToggle = document.getElementById('navbarNav')
 const bsCollapse = new bootstrap.Collapse(menuToggle)
+addEventListener('load', () => { bsCollapse.hide() })
+
 navLinks.forEach((l) => {
-    l.addEventListener('click', () => { bsCollapse.toggle() })
+    l.addEventListener('click', () => { bsCollapse.hide() })
 })
