9aa17bb3e177683e9cbf097e0b5e3a930e21df72
bernkastel15 <redpangilinan715@gmail.com>
2022-11-02 06:58:45 +0800

    Update function.js

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,7 +1,8 @@
 const navLinks = document.querySelectorAll('.nav-item')
 const menuToggle = document.getElementById('navbarNav')
 const bsCollapse = new bootstrap.Collapse(menuToggle)
-addEventListener('scroll', () => { bsCollapse.hide() })
+window.addEventListener('load', () => { bsCollapse.hide() })
+window.addEventListener('scroll', () => { bsCollapse.hide() })
 navLinks.forEach((l) => {
     l.addEventListener('click', () => { bsCollapse.hide() })
 })
