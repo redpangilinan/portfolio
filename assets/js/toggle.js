825019dc531667dedd004d719462068a7d4d66cf
bernkastel15 <redpangilinan715@gmail.com>
2022-11-02 06:21:12 +0800

    Website design tweak + AOS integration

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,6 +1,7 @@
 const navLinks = document.querySelectorAll('.nav-item')
 const menuToggle = document.getElementById('navbarNav')
 const bsCollapse = new bootstrap.Collapse(menuToggle)
+addEventListener('load', () => { bsCollapse.toggle() })
 navLinks.forEach((l) => {
     l.addEventListener('click', () => { bsCollapse.toggle() })
 })
